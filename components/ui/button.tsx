8652c142ac151b80553d9b8ade0f2e74699daf99
Ryan L <rliu2510@gmail.com>
2026-01-16 11:31:22 +1000

    styling changes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline: "border border-input hover:bg-muted-background",
-        secondary: "bg-secondary hover:bg-input",
+        secondary: "bg-secondary hover:bg-input/70",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -94,7 +94,7 @@ function Calendar({
           "text-muted-foreground rounded-md flex-1 font-normal text-[0.8rem] select-none",
           defaultClassNames.weekday
         ),
-        week: cn("flex w-full mt-2", defaultClassNames.week),
+        week: cn("flex w-full mt-1", defaultClassNames.week),
         week_number_header: cn(
           "select-none w-(--cell-size)",
           defaultClassNames.week_number_header
