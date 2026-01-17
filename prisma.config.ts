c3b0fd67dd56d555111891184d527694da531c7c
Ryan L <rliu2510@gmail.com>
2026-01-17 22:04:17 +1000

    added env variable error for typescript

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url:
+      process.env["DATABASE_URL"] ??
+      (() => {
+        throw new Error("DATABASE_URL environment variable is not set");
+      })(),
   },
 });
