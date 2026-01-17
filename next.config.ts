336af44c2b3df453c427ea7e9ce171da2bbb9ed2
Ryan L <rliu2510@gmail.com>
2026-01-17 23:07:52 +1000

    removed workaround package, failed

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
