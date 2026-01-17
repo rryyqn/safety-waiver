ea0dee805e6e4375becd052258c377e04761017e
Ryan L <rliu2510@gmail.com>
2026-01-18 08:51:20 +1000

    updated file tracing at build

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
-    "/*": ["./node_modules/.prisma/client/**/*"],
+    "/**/*": ["./prisma/generated/**/*"],
   },
 };
 
